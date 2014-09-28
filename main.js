511bea31c2f28bc05b95e38a1d73e6c616d3a0ab
Greg Ferrell <gregerg@gmail.com>
2014-09-27 18:25:31 -0700

    added buttons

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 R.ready(function() {
   R.player.play({source: "a3032151"}); // Alice In Chains - The Devil Put Dinosaurs Here
+  R.player.on("change:playingTrack", function(track) {
+    $('.artist').text('Artist: ' + track.get('artist'));
+    $('.title').text('Title: ' + track.get('name'));
+    $('.album').text('Album: ' + track.get('album'));
+  });
 });
